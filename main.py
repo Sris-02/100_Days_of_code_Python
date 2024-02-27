04b02126b84d5e03d6c8b854d0fb5ea45b236e5e
Sri <136840104+Sris-02@users.noreply.github.com>
2024-02-27 04:26:46 +0000

    updated
    adding comments

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+# writing print function
 print('Day 1 - Python Print Function')
 print('The function is declared like this:')
 print("print('what to print')")
+#adding blank lines
 print("\n\n")
 print("Hello World!\nHello World!\nHello World!")
+#adding blank lines
 print("\n\n")
+#string concatenation
 print("Hello"+" "+"Angel")
