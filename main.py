56f4926c91ed7a235f62715b222897ab7565833e
Sri <136840104+Sris-02@users.noreply.github.com>
2024-02-27 04:19:57 +0000

    updated python
    escape sequence character

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print('Day 1 - Python Print Function')
 print('The function is declared like this:')
 print("print('what to print')")
+print("\n\n")
+print("Hello World!\nHello World!\nHello World!")
