4d6c220ddc146c28a95ac4d78a8455c8ec3c1ee6
Sri <136840104+Sris-02@users.noreply.github.com>
2024-02-27 05:20:15 +0000

    updated
    variables in python

--- main.py
+++ main.py
@@ -2,22 +2,41 @@
 print('Day 1 - Python Print Function')
 print('The function is declared like this:')
 print("print('what to print')")
+
 # adding blank lines
 print("\n\n")
 print("Hello World!\nHello World!\nHello World!")
 # adding new lines using backslash n.
 print("\n\n")
+
 # string concatenation
 # string exercise
 print("Hello"+" "+"Angel")
+
 # String Manipulation
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print(("New lines can be created with a backslash and n."))
+
 # input function
 # nested inside the other i.e. input function inside the print function.
 print("Hello " + input("What is your name?"))
+
 # input exercise:)
 print(len(input("What is your name? ")))
+print("\n\n")
 
+# Python variables
+name = input("What is your name?")
+print(name)
+
+name = "jack"
+print(name)
+
+name = "Arnav"
+print(name)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
