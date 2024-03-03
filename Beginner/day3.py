392bb96fbb32f37dc6fbda80e5adffa394eed85d
Sri <136840104+Sris-02@users.noreply.github.com>
2024-03-03 16:22:08 +0000

    update
    -

--- Beginner/day3.py
+++ Beginner/day3.py
@@ -64,4 +64,28 @@ else:
   print("Leap year. ")
 
 # Multiple if statements in succession.
+print("Welcome to the rollercoaster! ")
+height = int(input("What is your height in cm? "))
+bill = 0
+if height >= 120:
+  print("You can ride the rollercoaster! ")
+  age = int(input("What is your age? "))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5. ")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7. ")
+  else:
+    bill = 12
+    print("Adult tickets are $12. ")
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    print(f"Your final bill is ${bill + 3}.")
+else:
+  print("Sorry, you have to grow taller beforre you can ride. ")
 
+# Pizza order exercise :
+print("Welcome to Python Pizza Deliveries! ")
+size = input("What size pizza do you want? S, M, or L.")
+add_pepperoni = input("Do you want pepperoni? Y or N. ")
--- README.md
+++ README.md
@@ -308,4 +308,36 @@ else:
   print("Leap year. ")
 ```
 
+#Multiple if statements in succession -
 
+fun exercise to go ahead that include the bill payment according to, whether photo taken or not.
+
+```
+print("Welcome to the rollercoaster! ")
+height = int(input("What is your height in cm? "))
+
+bill = 0
+
+if height >= 120:
+  print("You can ride the rollercoaster! ")
+  age = int(input("What is your age? "))
+  
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5. ")
+    
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7. ")
+    
+  else:
+    bill = 12
+    print("Adult tickets are $12. ")
+    
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    print(f"Your final bill is ${bill + 3}.")
+    
+else:
+  print("Sorry, you have to grow taller beforre you can ride. ")
+```
